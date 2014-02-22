55d2d2bf715793013e1d8250f316dd3006ce279d
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-02-22 06:52:28 -0800

    adding actual link

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
             main : {
                 src : 'templates/README.template.md',
                 dest : 'README.md',
-                baseLinkPath : 'http://test.com'
+                baseLinkPath : 'https://github.com/pajtai/grunt-release-notes/tree/master/release_notes/'
             }
         },
         warning : 'Do not modify directly. This file is compiled from a template.'
